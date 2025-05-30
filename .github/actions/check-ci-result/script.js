a219b13da79bda79be2febd84957e3bf499cd09b
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2025-05-30 21:00:46 +0900

    wip

--- .github/actions/check-ci-result/script.js
+++ .github/actions/check-ci-result/script.js
@@ -9,32 +9,54 @@
 
 /** @type {(arg: { github: { rest: OctokitClient }, context: WorkflowRunContext }) => Promise<void>} */
 module.exports = async ({ github, context }) => {
+  const sha = context.payload.workflow_run?.head_sha || context.sha;
+  const statusContext = process.env.STATUS_CONTEXT;
   const checks = await github.rest.checks.listForRef({
     ref: context.payload.workflow_run.head_branch,
     owner: context.repo.owner,
     repo: context.repo.repo,
   });
-
-  const notCompletedRuns = checks.data.check_runs.filter(
+  const allRuns = checks.data.check_runs;
+  const notCompletedRuns = allRuns.filter(
     (suite) => suite.status !== "completed"
   );
 
-  if (notCompletedRuns.length === 0) {
-    const sha = context.payload.workflow_run.head_sha;
+  switch (context.eventName) {
+    case "pull_request_review": {
+      if (allRuns.length === 0) {
+        console.log(
+          "No runs found, assuming CI is skipped with commit comment"
+        );
 
-    await github.rest.repos.createCommitStatus({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      sha,
-      context: "foo bar",
-      state: checks.data.check_runs.every(
-        (suite) => suite.conclusion === "success"
-      )
-        ? "success"
-        : "failure",
-    });
-  } else {
-    console.log("Not all runs are completed");
-    console.log(notCompletedRuns.map((s) => s.url));
+        await github.rest.repos.createCommitStatus({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          sha,
+          context: statusContext,
+          state:
+            context.payload.review.state === "approved" ? "success" : "failure",
+        });
+      }
+      return;
+    }
+    case "workflow_run": {
+      if (notCompletedRuns.length === 0) {
+        await github.rest.repos.createCommitStatus({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          sha,
+          context: statusContext,
+          state: checks.data.check_runs.every(
+            (suite) => suite.conclusion === "success"
+          )
+            ? "success"
+            : "failure",
+        });
+      } else {
+        console.log("Not all runs are completed");
+        console.log(notCompletedRuns.map((s) => s.url));
+      }
+      return;
+    }
   }
 };
